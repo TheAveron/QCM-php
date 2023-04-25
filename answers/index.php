71679ee879d58b336fcbdb40e54f4feed0c33e2d
TheAveron <77922089+TheAveron@users.noreply.github.com>
2023-04-25 20:18:42 +0200

    Feat :sparkles: : Working questionary

---
+++ answers/index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html>
+	<head>
+		<title>QCM Victor Duruy et NSI</title>
+		<meta charset="utf-8"/>
+		<link href='../style.css' rel='stylesheet'>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	</head>
+	<body>
+		<h1>Réponses du QCM<h1>
+		<h2>Créateur : Victor A.</h2>
+
+		<?php
+			$questions = file_get_contents('../questions.json');
+			$questions = json_decode($questions, true);
+
+			$points=0;
+
+			foreach ($questions as $question) {
+				foreach ($question['reponses'] as $answer)
+				{
+					if (isset($answer[$_POST[$question['name']][0]]) and $answer[$_POST[$question['name']][0]]==1)
+					{
+						$points++;
+					}
+				}
+			}
+			echo "<p>Points : $points</p>"
+		?>
+
+		<a href="/">Retour</a>
+	<body>
+</html>
--- index.php
+++ index.php
@@ -15,33 +15,28 @@
 			$questions = json_decode($questions, true);
 
 			$compteur=1;
+			echo '<form action="answers/index.php" method="post">';
+
 			foreach($questions as $question)
 			{
 
-				echo '<form method="post">';
+				echo '<div class="question_box">';
 				echo '<h3>'.$question['question'].'</h3>';
-				echo '<label for="option'.$compteur.'"><input class="checkbox" type="checkbox" name="'.$question['name'].'[]" id="1" value="'.array_keys($question['reponses'][1])[0].'">'.array_keys($question['reponses'][1])[0].'</label><br>';
-				echo '<label for="option'.$compteur.'"><input class="checkbox" type="checkbox" name="'.$question['name'].'[]" id="2" value="'.array_keys($question['reponses'][2])[0].'">'.array_keys($question['reponses'][2])[0].'</label><br>';
-				echo '<label for="option'.$compteur.'"><input class="checkbox" type="checkbox" name="'.$question['name'].'[]" id="3" value="'.array_keys($question['reponses'][3])[0].'">'.array_keys($question['reponses'][3])[0].'</label><br>';
-				echo '<input class="submit" type="submit" name="submit'.$compteur.'" value="Submit'.$compteur.'">';
-				echo '</form>';
-
-				$compteur+=1;
-
-			}
-		?>
-
-		<?php
-			if(isset($_POST['submit1'])){
-				if(!empty($_POST['proviseur'])) {
-					foreach($_POST['proviseur'] as $selected) {
-						echo "<p>You selected " . $selected . "</p>";
-					}
-				} else {
-					echo "<p>Please select at least one option.</p>";
+				for ($answers = 0; $answers<=2; $answers++)
+				{
+					echo '<label for="question '.$compteur.'"><input class="checkbox" type="checkbox" name="'.$question['name'].'[]" id="'.($answers+1).'" value="'.array_keys($question['reponses'][$answers])[0].'"> <div class="text_ans">'.array_keys($question['reponses'][$answers])[0].'</div></label><br>';
 				}
+				
+				
+				echo 'submit'.$compteur;
+				$compteur+=1;
+				echo '</div>';
 			}
+			echo '<input class="submit" type="submit" value="Valider">';
+			echo '</form>';
 		?>
+
+		<a href="answers">Réponses</a>
 	<body>
 </html>
 		
